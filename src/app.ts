ac3290c6ea8845f32dd31e4b3726612bfe60897b
Devin Walter <devin.m.walter4@gmail.com>
2020-10-24 16:33:46 -0500

    connected to database and stores sessions

--- src/app.ts
+++ src/app.ts
@@ -4,28 +4,84 @@ import compression from "compression";
 import session from "express-session";
 import cors from "cors";
 import errorHandler from "errorhandler";
+import mongo from "connect-mongo";
+import mongoose from "mongoose";
+import bluebird from "bluebird";
 import dotenv from "dotenv";
-dotenv.config({ path: ".env" });
+dotenv.config();
+
+interface DbConnection {
+  name: string;
+  password: string;
+  user: string;
+}
 
 class App {
-    private appSession: express.Application;
+  private appSession: express.Application;
+  private port: number;
+  private sessionSecret: string;
+  private dbConnectionInfo: DbConnection;
+  private mongoUri: string;
+  private MongoStore: mongo.MongoStoreFactory;
 
-    constructor() {
-        this.appSession = express();
-        this.configureMiddlewares();
-    }
+  constructor() {
+    this.appSession = express();
+    this.port = (process.env.PORT as unknown) as number;
+    this.dbConnectionInfo = {
+      name: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      user: process.env.DB_USER,
+    };
+    this.buildDbUri();
+    this.connectToDatabase();
+    this.MongoStore = mongo(session);
+    this.configureMiddlewares();
+  }
 
-    private configureMiddlewares() {
-        this.appSession.use(compression());
-        this.appSession.use(express.json());
-        this.appSession.use(express.urlencoded({ extended: true }));
-        this.appSession.use(cors());
-        this.appSession.use(errorHandler);
-    }
+  private configureMiddlewares() {
+    this.appSession.use(compression());
+    this.appSession.use(express.json());
+    this.appSession.use(express.urlencoded({ extended: true }));
+    this.appSession.use(cors());
+    this.appSession.use(
+      session({
+        resave: true,
+        saveUninitialized: true,
+        secret: this.sessionSecret,
+        store: new this.MongoStore({
+          url: this.mongoUri,
+          autoReconnect: true,
+        }),
+      }),
+    );
+    this.appSession.use(errorHandler);
+  }
 
-    public listen() {
-        this.appSession.listen(4000, () => console.log("Listening on port 4000"));
+  private buildDbUri(): void {
+    const { name, user, password } = this.dbConnectionInfo;
+    this.mongoUri = `mongodb+srv://${user}:${password}@cluster0-xcrfk.mongodb.net/${name}?retryWrites=true&w=majority`;
+  }
+
+  private async connectToDatabase(): Promise<void> {
+    try {
+      mongoose.Promise = bluebird;
+      await mongoose.connect(this.mongoUri, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      });
+
+      console.log("MongoDB Connected and ready to use");
+    } catch (error) {
+      console.error(error);
     }
+  }
+
+  public listen(): void {
+    this.appSession.listen(this.port, () =>
+      console.log(`Listening on port ${this.port}`),
+    );
+  }
 }
 
 export default App;
